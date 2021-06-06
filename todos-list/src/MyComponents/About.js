874c66c7ff3385df165fe758774a03f94045b696
rocky saini <rockysainig@gmail.com>
2021-06-06 22:26:16 +0530

    Initial commit

---
+++ todos-list/src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            this is an component
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quidem saepe earum consectetur natus quisquam reprehenderit asperiores eveniet laboriosam animi dicta iusto vitae nobis harum, expedita fuga hic, cum ipsam. Pariatur exercitationem aperiam velit?</p>
+        </div>
+    )
+}
